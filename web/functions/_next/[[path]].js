0486a1b0f511af38eaeb7fd44f3b5519d08c6c8c
vico-renxo <amigosxsiempre1987@gmail.com>
2026-07-02 09:53:41 -0500

    Delete web/functions/_next/[[path]].js

--- web/functions/_next/[[path]].js
+++
@@ -1,7 +0,0 @@
-// CF Pages Function: intercepta /_next/* y sirve desde /transporte/_next/*
-// (Next.js con basePath:'/transporte' pone los assets en /transporte/_next/)
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.pathname = '/transporte' + url.pathname;
-  return context.env.ASSETS.fetch(url.toString());
-}
